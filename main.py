34582514670fd1bf54d7a5f86ab1feba8c9cb5cd
UrielLiannLima <urielliannlima@gmail.com>
2023-10-14 17:06:11 -0300

    main.py atualizada
    Agora printa head e tail do dataframe

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import limpar_csv
-import pandas
+import limpar_df as ldf
+import pandas as pd
 
 indexes = input("Nome do arquivo de indexação (.txt): ")
-dataframe = input("Nome do dataset (.csv): ")
+df = ldf.open_df(input("Arquivo de dados (.csv): "))
 
-limpar_csv.drop_cols(indexes, dataframe)
+clean_df = ldf.remove_outliers(ldf.drop_cols(indexes, df))
+clean_df.to_csv("clean_data.csv", index=False)
+
+print(clean_df.head())
+print(clean_df.tail())
