fee7749832a95e77e9eec6e7778a69e0af4adf7d
UrielLiannLima <urielliannlima@gmail.com>
2023-10-16 08:24:14 -0300

    doctests e unittests refeitos

--- limpar_df.py
+++ limpar_df.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os
+import doctest
+import unittest
 
 def open_df (data_file: str):
     """
@@ -15,12 +18,11 @@ def open_df (data_file: str):
         Retorna o dataframe aberto
     
     >>> # Teste normal
-    >>> with open("teste.csv", "w") as file: file.write("A;B;C\\n1;2;3\\n4;5;6")
-    >>> df = open_df("teste.csv")
-    >>> df.shape
-    (2, 3)
-    >>> import os
-    >>> os.remove("teste.csv")
+    >>> df = open_df("arquivos_doctest/teste.csv")
+    
+    >>> # Teste com inexistente
+    >>> df = open_df("inexistente.csv")
+    Arquivo não encontrado: [Errno 2] No such file or directory: 'inexistente.csv'
     """
     try:
         df = pd.read_csv(data_file, encoding="unicode_escape", engine="python", on_bad_lines="skip", sep=";")
@@ -54,34 +56,9 @@ def drop_cols (text_file: str, df):
     df : Dataframe Pandas
         Dataframe com as colunas especificadas no arquivo de texto dropadas
     
-    >>> # Teste normal da função
-    >>> df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
-    >>> with open("index.txt", "w") as file:
-    ...     file.write("A\\nC")
-    >>> new_df = drop_cols("index.txt", df)
-    >>> new_df.columns.to_list()
+    # Exemplo
+    >>> drop_cols("arquivos_doctest/index.txt", pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})).columns.to_list()
     ['B']
-    >>> import os
-    >>> os.remove("index.txt")
-    
-    >>> # Teste com um index vazio
-    >>> empty_index = ""
-    >>> with open("empty_index.txt", "w") as file:
-    ...     file.write(empty_index)
-    >>> new_df = drop_cols("empty_index.txt", df)
-    >>> new_df.columns.to_list()
-    ['A', 'B', 'C']
-    >>> import os
-    >>> os.remove("empty_index.txt")
-    
-    >>> # Teste de index com colunas que não existem
-    >>> with open("invalid_index.txt", "w") as file:
-    ...     file.write("X\\nY")
-    >>> new_df = drop_cols("invalid_index.txt", df)
-    >>> new_df.columns.to_list()
-    ['A', 'B', 'C']
-    >>> import os
-    >>> os.remove("invalid_index.txt")
     """
     try:
         with open(text_file) as indexes:
@@ -112,16 +89,11 @@ def remove_outliers (df):
         Retorna o dataframe, porém com as linhas que possuem o código de outlier removidas.
     
     >>> # Teste normal
-    >>> df = pd.DataFrame({"A": [1, 2, 3, 88888], "B": [4, 5, 6, 7]})
-    >>> cleaned_df = remove_outliers(df)
-    >>> cleaned_df.shape
-    (3, 2)
+    >>> cleaned_df = remove_outliers(pd.DataFrame({"A": [1, 2, 3, 88888], "B": [4, 5, 6, 7]}))
     
     >>> # Teste sem outliers
-    >>> df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
-    >>> cleaned_df = remove_outliers(df)
-    >>> cleaned_df.shape
-    (3, 2)
+    >>> cleaned_df = remove_outliers(pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}))
+
     """
     try:
         outlier_code = 88888
@@ -132,43 +104,39 @@ def remove_outliers (df):
         return None
 
 # Unit test para o módulo
-def testar_open_df ():
-    # Cria um csv para o teste utilizando dados de amostra
-    amostra = "A;B;C\n1;2;3\n4;5;6"
-    with open("teste.csv", "w") as file:
-        file.write(amostra)
-
-    df = open_df("teste.csv")
-    assert df.shape == (2, 3)
-
-    # Remove o arquivo usado no teste
-    import os
-    os.remove("teste.csv")
-
-def testar_drop_cols ():
-    # Cria um dataframe de teste e um arquivo de texto para os indexes
-    df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
-    with open("index.txt", "w") as file:
-        file.write("A\nC")
-
-    new_df = drop_cols("index.txt", df)
-    assert new_df.columns.to_list() == ["B"]
-
-    # Remove o arquivo usado no teste
-    import os
-    os.remove("index.txt")
-
-def testar_remove_outliers ():
-    # Cria um dataframe de exemplo com o código de outlier
-    df = pd.DataFrame({"A": [1, 2, 3, 88888], "B": [4, 5, 6, 7]})
-
-    cleaned_df = remove_outliers(df)
-    assert cleaned_df.shape == (3, 2)
-
+class teste_funcoes(unittest.TestCase):
+    
+    def testar_open_df (self):
+        # Cria um csv para o teste utilizando dados de amostra
+        amostra = "A;B;C\n1;2;3\n4;5;6"
+        with open("teste.csv", "w") as file:
+            file.write(amostra)
+    
+        df = open_df("teste.csv")
+        self.assertEqual(df.shape, (2, 3))
+    
+        # Remove o arquivo usado no teste
+        os.remove("teste.csv")
+    
+    def testar_drop_cols (self):
+        # Cria um dataframe de teste e um arquivo de texto para os indexes
+        df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
+        with open("index.txt", "w") as file:
+            file.write("A\nC")
+    
+        new_df = drop_cols("index.txt", df)
+        self.assertEqual(new_df.columns.to_list(), ["B"])
+    
+        # Remove o arquivo usado no teste
+        os.remove("index.txt")
+    
+    def testar_remove_outliers (self):
+        # Cria um dataframe de exemplo com o código de outlier
+        df = pd.DataFrame({"A": [1, 2, 3, 88888], "B": [4, 5, 6, 7]})
+    
+        cleaned_df = remove_outliers(df)
+        self.assertEqual(cleaned_df.shape, (3, 2))
+    
 if __name__ == "__main__":
-    testar_open_df()
-    testar_drop_cols()
-    testar_remove_outliers()
-    import doctest
+    unittest.main()
     doctest.testmod()
-    print("Teste Completo.")
