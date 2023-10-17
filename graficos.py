5647d8a40b350ad85b3d116fa84b18ad8d46d945
Vitor Palmeira <126432167+vitorpalmeira@users.noreply.github.com>
2023-10-17 13:20:35 -0300

    Gráficos Feito

--- graficos.py
+++ graficos.py
@@ -1,36 +1,78 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import limpar_df as ldf
-import pandas as pd
-
-# NO_REGIAO
-# IN_AGUA_INEXISTENTE
-# IN_ENERGIA_INEXISTENTE
-# IN_ESGOTO_INEXISTENTE
-# IN_LIXO_SERVICO_COLETA
-# IN_BANHEIRO
-# IN_BIBLIOTECA
-# IN_LABORATORIO_INFORMATICA
-# IN_QUADRA_ESPORTES
-# IN_SALA_DIRETORIA
-# IN_SALA_PROFESSOR
-# IN_SECRETARIA
-# IN_ACESSIBILIDADE_INEXISTENTE
-# IN_ALIMENTACAO
-# IN_MATERIAL_PED_NENHUM
 
 # Abrir o arquivo de dados e estruturação
 df = ldf.open_df("data.csv")
 structure_df = ldf.remove_outliers(ldf.drop_cols("indexes/custom.txt", ldf.drop_cols("indexes/base.txt", df)))
 
-# Busca os dados únicos na coluna
+# Busca as regiões do país
 regioes = structure_df["NO_REGIAO"].unique()
 
-coluna_desejada = 'palavras'
-contagem_palavras = dados[coluna_desejada].value_counts()
-palavra_alvo = 'exemplo'
-contagem_palavra_alvo = dados[coluna_desejada].str.count(palavra_alvo).sum()
-print(contagem_palavras)
+marcacao = {
+    "IN_AGUA_INEXISTENTE": "Água",
+    "IN_ENERGIA_INEXISTENTE": "Energia",
+    "IN_ESGOTO_INEXISTENTE": "Esgotamento Sanitário",
+    "IN_LIXO_SERVICO_COLETA": "Coleta de Lixo",
+    "IN_BANHEIRO": "Banheiro",
+    "IN_BIBLIOTECA": "Biblioteca",
+    "IN_LABORATORIO_INFORMATICA": "Laboratório de Informática",
+    "IN_QUADRA_ESPORTES": "Quadra de Esportes",
+    "IN_SALA_DIRETORIA": "Sala de Diretoria",
+    "IN_SALA_PROFESSOR": "Sala de Professores",
+    "IN_SECRETARIA": "Secretaria",
+    "IN_ACESSIBILIDADE_INEXISTENTE": "Recursos de Acessibilidade",
+    "IN_ALIMENTACAO": "Alimentação",
+    "IN_MATERIAL_PED_NENHUM": "Materiais Pedagógicos"
+}
 
+# Calcula o número total de escolas por região
+total_escolas_por_regiao = [len(structure_df[structure_df["NO_REGIAO"] == regiao]) for regiao in regioes]
+
+# Itera todas as regiões
+for i, regiao in enumerate(regioes):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    
+    labels = []
+    qtde_infra = []
+
+    # Adiciona a label com o número total de escolas por região
+    labels.append(f"Escolas Totais: {total_escolas_por_regiao[i]}")
+    qtde_infra.append(total_escolas_por_regiao[i])
+
+    for column, label in marcacao.items():
+        reg_df = structure_df[structure_df["NO_REGIAO"] == regiao]
+        total_escolas = len(reg_df)  # Total de escolas na região
+        value = reg_df[column].sum()
+        
+        #Corrige os dados que vinham invertidos, com o final "_INEXISTENTE"
+        if label in ["Água", "Energia", "Esgotamento Sanitário", "Recursos de Acessibilidade"]:
+            value = total_escolas - value
+
+        # Adiciona o número total para cada label
+        labels.append(f"{label} - Escolas: {value}")
+        qtde_infra.append(value)
+
+    index = np.arange(len(labels))
+
+    ax.bar(
+        index,
+        qtde_infra,
+    )
+
+    ax.set_xlabel("Infraestrutura")
+    ax.set_ylabel("Nº de Escolas")
+    ax.set_title(f"Distribuição de Infraestrutura por Região: {regiao}")
+
+    ax.set_xticks(index)
+    ax.set_xticklabels(labels, rotation=45, ha='right')
+
+    plt.tight_layout()
+
+    # Salva o gráfico para cada região
+    plt.savefig(f'{regiao}_distribution.png')
+
+    # Mostra o plot
+    plt.show()
 
 
